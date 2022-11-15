6bf09895057d754e40ab2f33b13e0f535afdec73
wahab202 <abdulwahabaziz20@gmail.com>
2022-11-15 22:24:43 +0500

    changed project to swift

---
+++ YassirMovieAssessment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YassirMovieAssessment
+//
+//  Created by Abdul Wahab on 15/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
